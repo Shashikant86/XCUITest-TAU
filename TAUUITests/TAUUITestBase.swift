c654b1cca582b4e9e40c2251b0a890a79c7f68d5
Shashikant86 <shashikant.jagtap@icloud.com>
2019-02-10 18:01:26 +0000

    Finished Chapter 3

---
+++ TAUUITests/TAUUITestBase.swift
@@ -0,0 +1,28 @@
+//
+//  TAUUITestBase.swift
+//  TAUUITests
+//
+//  Created by Shashikant Jagtap on 10/02/2019.
+//  Copyright © 2019 Shashikant Jagtap. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class TAUUITestBase: XCTestCase {
+    
+    var app = XCUIApplication()
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launch()
+       
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        app.terminate()
+     
+    }
+}
--- TAUUITests/TAUUITests.swift
+++ TAUUITests/TAUUITests.swift
@@ -8,27 +8,9 @@
 
 import XCTest
 
-class TAUUITests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+class TAUUITests: TAUUITestBase {
 
     func testAllElementsOfMainScreen() {
-        
-        let app = XCUIApplication()
         app/*@START_MENU_TOKEN@*/.staticTexts["welcomeMessage"]/*[[".staticTexts[\"Welcome to XCUITest Course \"]",".staticTexts[\"welcomeMessage\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app/*@START_MENU_TOKEN@*/.staticTexts["enterCity"]/*[[".staticTexts[\"Enter Your City \"]",".staticTexts[\"enterCity\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         app/*@START_MENU_TOKEN@*/.buttons["enrollButton"]/*[[".buttons[\"Enroll\"]",".buttons[\"enrollButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
@@ -37,18 +19,9 @@ class TAUUITests: XCTestCase {
     }
     
     func testThankYouMessage() {
-        
-        let app = XCUIApplication()
         app.textFields["city"].tap()
         app.textFields["city"].typeText("London")
         app/*@START_MENU_TOKEN@*/.buttons["enrollButton"]/*[[".buttons[\"Enroll\"]",".buttons[\"enrollButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         XCTAssertTrue(app.staticTexts["Thanks for Joining!"].exists)
     }
-    
-    
-    
-    
-    
-    
-
 }
