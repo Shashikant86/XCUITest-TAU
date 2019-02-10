663f49681f005b3ae4ffe3c65d95b5bb23c056a0
Shashikant86 <shashikant.jagtap@icloud.com>
2019-02-10 16:47:31 +0000

    Finished Chapter 2

---
+++ TAUUITests/TAUUITests.swift
@@ -0,0 +1,54 @@
+//
+//  TAUUITests.swift
+//  TAUUITests
+//
+//  Created by Shashikant Jagtap on 10/02/2019.
+//  Copyright © 2019 Shashikant Jagtap. All rights reserved.
+//
+
+import XCTest
+
+class TAUUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testAllElementsOfMainScreen() {
+        
+        let app = XCUIApplication()
+        app/*@START_MENU_TOKEN@*/.staticTexts["welcomeMessage"]/*[[".staticTexts[\"Welcome to XCUITest Course \"]",".staticTexts[\"welcomeMessage\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["enterCity"]/*[[".staticTexts[\"Enter Your City \"]",".staticTexts[\"enterCity\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["enrollButton"]/*[[".buttons[\"Enroll\"]",".buttons[\"enrollButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.images["TAUlogo"].tap()
+        XCTAssertTrue(app.staticTexts["Please Enter City"].exists)
+    }
+    
+    func testThankYouMessage() {
+        
+        let app = XCUIApplication()
+        app.textFields["city"].tap()
+        app.textFields["city"].typeText("London")
+        app/*@START_MENU_TOKEN@*/.buttons["enrollButton"]/*[[".buttons[\"Enroll\"]",".buttons[\"enrollButton\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.staticTexts["Thanks for Joining!"].exists)
+    }
+    
+    
+    
+    
+    
+    
+
+}
